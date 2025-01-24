478a704129813f6213610556f3e9788b2f28fff1
hsvillaz <hsvillaz@gmail.com>
2025-01-24 20:00:38 +0000

    06

--- employees/webapp/controller/MainView.controller.js
+++ employees/webapp/controller/MainView.controller.js
@@ -2,63 +2,41 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/Filter",
-    "sap/ui/model/FilterOperator"
+    "sap/ui/model/FilterOperator",
+    "sap/m/MessageToast",
 ],
     /**
     * @param {typeof sap.ui.core.mvc.Controller} Controller
     * @param {typeof sap.ui.model.Filter} Filter
     * @param {typeof sap.ui.model.FilterOperator} FilterOperator
+    * @param {typeof sap.m.MessageToast} MessageToast
     */
 
-    (Controller, Filter, FilterOperator) => {
+    (Controller, Filter, FilterOperator, MessageToast) => {
         "use strict";
 
         function onInit() {
 
             var oJSONModel = new sap.ui.model.json.JSONModel();
             var oView = this.getView();
-            // var i18nBundle = oView.getModel("i18n").getResourceBundle();
             var i18nBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
 
-            // var oJSON = {
-            //     employeeId: "12345",
-            //     countryKey: "UK",
-            //     listCountry: [
-            //         {
-            //             key: "US",
-            //             text: i18nBundle.getText("countryUS")
-            //         },
-            //         {
-            //             key: "UK",
-            //             text: i18nBundle.getText("countryUK")
-            //         },
-            //         {
-            //             key: "ES",
-            //             text: i18nBundle.getText("countryES")
-            //         }
-            //     ]
-            // };
-            // oJSONModel.setData(oJSON);
             oJSONModel.loadData("./localService/mockdata/Employees.json", false);
-            // oJSONModel.attachRequestCompleted(function(oEventModel){
-            //     console.log(JSON.stringify(oJSONModel.getData()));
-            // });
             oView.setModel(oJSONModel);
         };
 
         function onFilter() {
 
             var oJSON = this.getView().getModel().getData();
-
             var filters = [];
 
             if (oJSON.EmployeeId !== "") {
                 filters.push(new Filter("EmployeeID", FilterOperator.EQ, oJSON.EmployeeId));
-            }
+            };
 
             if (oJSON.CountryKey !== "") {
                 filters.push(new Filter("Country", FilterOperator.EQ, oJSON.CountryKey));
-            }
+            };
 
             var oList = this.getView().byId("tableEmployee");
             var oBinding = oList.getBinding("items");
@@ -70,29 +48,23 @@ sap.ui.define([
             var oModel = this.getView().getModel();
             oModel.setProperty("/EmployeeId", "");
             oModel.setProperty("/CountryKey", "");
-        }
+        };
 
-        // function myCheck() {
-        //     var inputEmployee = this.byId("inputEmployee");
-        //     var valueEmployee = inputEmployee.getValue();
+        function showPostalCode(oEvent){
 
-        //     if (valueEmployee.length === 6) {
-        //         //inputEmployee.setDescription("OK");
-        //         this.getView().byId("labelCountry").setVisible(true);
-        //         this.getView().byId("slCountry").setVisible(true);
-        //     } else {
-        //         //inputEmployee.setDescription("Not OK");
-        //         this.getView().byId("labelCountry").setVisible(false);
-        //         this.getView().byId("slCountry").setVisible(false);
-        //     };
-        // };
+            var itemPressed = oEvent.getSource();
+            var oContext = itemPressed.getBindingContext();
+            var objectContext = oContext.getObject();
+
+            MessageToast.show(objectContext.PostalCode);
+        };
 
         var Main = Controller.extend("logali.employees.controller.MainView", {});
 
-        // Main.prototype.onValidate = myCheck;
         Main.prototype.onInit = onInit;
         Main.prototype.onFilter = onFilter;
         Main.prototype.onClearFilter = onClearFilter;
+        Main.prototype.showPostalCode = showPostalCode;
 
         return Main;
     });
