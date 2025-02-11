a4efeec00c847480d593d9d5f8c5443e38bcfa19
hsvillaz <hsvillaz@gmail.com>
2025-02-11 01:00:29 +0000

    11 Layouts

---
+++ employees/webapp/controller/Main.controller.js
@@ -0,0 +1,55 @@
+//@ts-nocheck
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+    /**
+    * @param {typeof sap.ui.core.mvc.Controller} Controller
+    */
+
+    (Controller) => {
+        "use strict";
+
+        function onInit() {
+
+            var oView = this.getView();
+
+            var oJSONModelEmpl = new sap.ui.model.json.JSONModel();
+            oJSONModelEmpl.loadData("./localService/mockdata/Employees.json", false);
+            oView.setModel(oJSONModelEmpl, "jsonEmployees");
+
+            var oJSONModelCountries = new sap.ui.model.json.JSONModel();
+            oJSONModelCountries.loadData("./localService/mockdata/Countries.json", false);
+            oView.setModel(oJSONModelCountries, "jsonCountries");
+
+            var oJSONModelLayout = new sap.ui.model.json.JSONModel();
+            oJSONModelLayout.loadData("./localService/mockdata/Layout.json", false);
+            oView.setModel(oJSONModelLayout, "jsonLayout");
+
+            var oJSONModelConfig = new sap.ui.model.json.JSONModel({
+                visibleID: true,
+                visibleName: true,
+                visibleCountry: true,
+                visibleCity: false,
+                visibleBtnShowCity: true,
+                visibleBtnHideCity: false
+            });
+            oView.setModel(oJSONModelConfig, "jsonModelConfig");
+
+            this._bus = sap.ui.getCore().getEventBus();
+            this._bus.subscribe("flexible", "showEmployee", this.showEmployeeDetails, this);
+        };
+
+        function showEmployeeDetails(category, nameEvent, path) {
+
+            var detailView = this.getView().byId("detailEmployeeView");
+            detailView.bindElement("jsonEmployees>" + path);
+            this.getView().getModel("jsonLayout").setProperty("/ActiveKey", "TwoColumnsMidExpanded");
+        };
+
+        var Main = Controller.extend("logali.employees.controller.Main", {});
+
+        Main.prototype.onInit = onInit;
+        Main.prototype.showEmployeeDetails = showEmployeeDetails;
+
+        return Main;
+    });
